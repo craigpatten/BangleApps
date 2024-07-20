e70261395122ee31cb0637a4f600cb6be6bb6905
Craig Patten <1353030+craigpatten@users.noreply.github.com>
2024-07-20 13:40:58 +0930

    lint

--- apps/daisy/app.js
+++ apps/daisy/app.js
@@ -1,6 +1,4 @@
 var SunCalc = require("suncalc"); // from modules folder
-const storage = require('Storage');
-const locale = require("locale");
 const widget_utils = require('widget_utils');
 const SETTINGS_FILE = "daisy.json";
 const LOCATION_FILE = "mylocation.json";
@@ -225,7 +223,7 @@ function drawClock() {
   var date = new Date();
   var timeStr = require("locale").time(date,1);
   var da = date.toString().split(" ");
-  var time = da[4].substr(0,5);
+  // var time = da[4].substr(0,5);
   var hhmm = timeStr.split(":")
   var hh = hhmm[0]; // da[4].substr(0,2);
   var mm = hhmm[1]; // da[4].substr(3,2);
